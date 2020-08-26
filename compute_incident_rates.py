fc4d9cc88ce1b79902430ccd930c0902010afbf0
Raman A. Shah <raman.anand.shah@gmail.com>
2020-08-26 10:34:32 -0400

    Back out funky usage of <5 records with Beta math.
    Replace it with a much simpler imputation of 2.5 events, leading to a
    considerably shorter code (and faster runtime).

--- compute_incident_rates.py
+++ compute_incident_rates.py
@@ -22,48 +22,20 @@ def impute_incidents(incidents_string):
 
 def interval_endpoint(row, cred_mass=0.9):
     '''Construct highest-posterior-density credible interval for the event rate.'''
-    incidents_string = row['incidents']
+    # Plug an imputed 2.5 events into the towns with <5 events
+    incidents = impute_incidents(row['incidents'])
     population = row['population']
-    if incidents_string == '<5':
-        # Posterior is a mixture of four betas for 1, 2, 3, and 4 incidents
-        iw = make_less_than_five_width(population, cred_mass=cred_mass)
-        left_mass = fmin(iw, 1.0 - cred_mass, ftol=1e-8, disp=False)[0]
-        return (less_than_five_ppf(left_mass, population),
-                less_than_five_ppf(left_mass + cred_mass, population))
-    else:
-        incidents = float(incidents_string)
-        iw = make_interval_width(incidents, population, cred_mass=cred_mass)
-        left_mass = fmin(iw, 1.0 - cred_mass, ftol=1e-8, disp=False)[0]
-        return (beta.ppf(left_mass, BETA_A + incidents, BETA_B + population),
-                beta.ppf(left_mass + cred_mass, BETA_A + incidents, BETA_B + population))
+    iw = make_interval_width(incidents, population, cred_mass=cred_mass)
+    left_mass = fmin(iw, 1.0 - cred_mass, ftol=1e-8, disp=False)[0]
+    return (beta.ppf(left_mass, BETA_A + incidents, BETA_B + population),
+            beta.ppf(left_mass + cred_mass, BETA_A + incidents,
+                     BETA_B + population))
 
 def make_interval_width(incidents, population, cred_mass=0.9):
     def interval_width(left_mass):
         return beta.ppf(left_mass + cred_mass, BETA_A + incidents, BETA_B + population) \
             - beta.ppf(left_mass, BETA_A + incidents, BETA_B + population)
     return interval_width
-
-def make_less_than_five_width(population, cred_mass=0.9):
-    def interval_width(left_mass):
-        return less_than_five_ppf(left_mass + cred_mass, population) \
-            - less_than_five_ppf(left_mass, population)
-    return interval_width
-
-def less_than_five_ppf(left_mass, population):
-    '''Shady.
-
-    Need to unpack the implicit assumptions in this unweighted sum or switch to
-    a Poisson likelihood.
-    '''
-    def less_than_five_cdf(x):
-        return 0.25 * (beta.cdf(x, BETA_A + 1, BETA_B + population) +
-                       beta.cdf(x, BETA_A + 2, BETA_B + population) +
-                       beta.cdf(x, BETA_A + 3, BETA_B + population) +
-                       beta.cdf(x, BETA_A + 4, BETA_B + population))
-    def less_than_five_error(x):
-        return less_than_five_cdf(x) - left_mass
-
-    return fsolve(less_than_five_error, beta.ppf(left_mass, BETA_A + 2, BETA_B + population))[0]
 
 # The SciPy function solvers and minimizers can throw off RuntimeWarnings in
 # typical use.
